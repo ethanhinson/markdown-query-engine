1a848f154a3f8688f8ad142c625bc19fd36505c1
ethanhinson <ethan@bluetent.com>
2024-11-18 15:05:59 -0700

    chore(): dont forget to initialize settings during index

--- MarkdownQueryEngine/bin/index.py
+++ MarkdownQueryEngine/bin/index.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+from llama_index.core import SummaryIndex
+from llama_index.core import Settings
 from llama_index.core.node_parser import SentenceSplitter
 from MarkdownQueryEngine.app.storage import get_storage_context
 from MarkdownQueryEngine.app.config import AppConfig
@@ -14,8 +17,13 @@ def index_documents(config: AppConfig):
     nodes = SentenceSplitter().get_nodes_from_documents(documents)
     storage_context = get_storage_context(config)
     storage_context.docstore.add_documents(nodes)
-    storage_context.persist(persist_dir="./storage")
+    summary_index = SummaryIndex(nodes, storage_context=storage_context)
+    summary_index.storage_context.persist()
+    vector_index = VectorStoreIndex(nodes, storage_context=storage_context)
+    vector_index.storage_context.persist()
 
 if __name__ == "__main__":
     config = AppConfig()
+    Settings.llm = config.get_llm()
+    Settings.embed_model = config.get_embed_model()
     index_documents(config)
